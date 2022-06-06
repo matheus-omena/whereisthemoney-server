6a43fbee96e07ce895b721d0e9e089de9efd17c0
Matheus Omena <omena.theus@live.com>
2022-06-06 10:41:46 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(3333, () => {
-    console.log(`Server running on port ${3333}`);
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
