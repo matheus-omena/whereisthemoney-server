5fd43a00e1c3945adb8c9e90cf8f246ad0b6aa69
Matheus Omena <omena.theus@live.com>
2022-07-25 12:29:44 -0300

    ajustes para deploy

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use('/', routes_1.routes);
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
